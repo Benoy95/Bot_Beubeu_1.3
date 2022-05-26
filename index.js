343787e4bf4889d682db903885d942960896ae6b
Benoy95 <benoitminor@gmail.com>
2022-05-26 19:08:47 +0200

    devil

--- index.js
+++ index.js
@@ -57,6 +57,9 @@ client.on("messageCreate",async message => {
     else if(message.content === prefix + "Michel?"){
         message.reply("Michel ou Michael ou encore DJ MICK! adore la musique MAIS SURTOUT LES VACANCES D'ETE !!, il est un peut con aussi...");
     }
+    else if(message.content === prefix + "Mathieu?"){
+        message.reply("Ce type est térrifiant <japanese_ogre:979430602841411596>");
+    }
     else if(message.content === prefix + "hymne"){
         message.channel.send(";;play https://www.youtube.com/watch?v=GLi-UF9U-RI&ab_channel=ScorpioMusic");
     }
