3f3627319b2631eea45d8f3fa15a074e7d5b76f6
Benoy95 <benoitminor@gmail.com>
2022-06-05 11:48:51 +0200

    qsc

--- index.js
+++ index.js
@@ -108,8 +108,10 @@ client.on("messageCreate",async message => {
      else if (message.content === prefix + "voc") {
         message.reply("oui");
         const voiceChannel = message.member.voice.channel;
+        message.reply("oui 1");
         voiceChannel.join()
              .then(connection => {
+                message.reply("oui 2");
                 const dispatcher = connection.play('./Benoit_Tourne.mp3', { volume: 0.5 });
              })
              .catch( message.reply("erreur"));
